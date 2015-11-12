0ef03b78a84dd67e2ea7518c814494ac6c97328b
Michael Chapman <mikechapman322@gmail.com>
2015-11-11 22:52:33 -0500

    eh

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  candy-shooter
+//
+//  Created by Michael Chapman on 11/11/15.
+//  Copyright © 2015 Michael Chapman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
