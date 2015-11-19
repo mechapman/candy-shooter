ef0deb68df3fd612976269977b2fb32c4ad14393
Michael Chapman <mikechapman322@gmail.com>
2015-11-18 23:28:19 -0500

    adjust threshold and size of mouthROI

--- main.cpp
+++ main.cpp
@@ -15,30 +15,37 @@
 using namespace std;
 using namespace cv;
 
-/** Function Headers */
+//Function Headers
 void detectAndDisplay( Mat frame );
 
-/** Global variables */
+// Global variables
+int threshold_value = 115;
+Mat mouthROI;
 
 String face_cascade_name = "/Users/mchapman/Downloads/opencv-3.0.0/data/haarcascades/haarcascade_frontalface_alt.xml";
 CascadeClassifier face_cascade;
 String window_name = "Capture";
 
-/** @function main */
 int main( void )
 {
     VideoCapture capture;
     Mat frame;
+
+    //Load the face cascade
+    if( !face_cascade.load( face_cascade_name ) ){
+        printf("--(!)Error loading face cascade\n");
+        return -1;
+    };
     
-    //-- 1. Load the face cascade
-    if( !face_cascade.load( face_cascade_name ) ){ printf("--(!)Error loading face cascade\n"); return -1; };
-    
-    //-- 2. Read the video stream
+    //Read the video stream
     capture.open( 0 );
     printf("opened webcam\n");
 
-    if ( ! capture.isOpened() ) { printf("--(!)Error opening video capture\n"); return -1; }
- 
+    if ( ! capture.isOpened() ) {
+        printf("--(!)Error opening video capture\n");
+        return -1;
+    }
+
     while ( capture.read(frame) )
     {
         if( frame.empty() )
@@ -46,19 +53,16 @@ int main( void )
             printf(" --(!) No captured frame -- Break!");
             break;
         }
-        
 
         //-- 3. Apply the classifier, detect open mouths
         detectAndDisplay( frame );
-        
         int c = waitKey(10);
         if( (char)c == 27 ) { break; } // escape
-        
+
     }
     return 0;
 }
 
-/** @function detectAndDisplay */
 void detectAndDisplay( Mat frame )
 {
     std::vector<Rect> faces;
@@ -80,15 +84,20 @@ void detectAndDisplay( Mat frame )
         Mat faceROI = frame_gray( faces[i] );
         
         //ROI where mouth will be
-        Rect mouth_rect = Rect(faces[i].x + (0.25*faces[i].width), faces[i].y + (0.6*faces[i].height), (0.5*faces[i].width), (0.35*faces[i].height));
+        Rect mouth_rect = Rect(faces[i].x + (0.35*faces[i].width), faces[i].y + (0.75*faces[i].height), (0.35*faces[i].width), (0.25*faces[i].height));
         
         //extract mouth from face
-        Mat mouthROI = frame_gray( mouth_rect );
+        mouthROI = frame_gray( mouth_rect );
         
-        threshold( mouthROI, mouthROI, 95, 255,0);
+        threshold( mouthROI, mouthROI, threshold_value, 255, 0);
         
         //display mouth ROI
         imshow("mouth", mouthROI);
+
+        //TODO:
+        // detect presence of open mouth
+        // draw circle around mouth in frame
+        // how to send signl via USB?
     }
 
     //-- Show what we got
